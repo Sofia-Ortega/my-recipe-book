faf4cb11479b12b1c9f5c7bfb2f15f52ee887295
Sofia-Ortega <s.ortega@tamu.edu>
2021-07-09 18:09:17 -0500

    makeStyles added to RecipeLayout

--- src/components/Recipe/RecipeLayout.js
+++ src/components/Recipe/RecipeLayout.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import {Link} from "react-router-dom";
 
-export default function RecipeLayout({cardDat}) {
-
-  const layoutStyle = {
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+  layoutStyle: {
     backgroundColor: "#f5f5f5",
     height: "100vh"
-  }
-
-  const appBarStyle = {
+  },
+  appBarStyle: {
     backgroundColor: "#ffffff"
-  }
-
-  const logoStyle = {
+  },
+  logoStyle: {
     display: "table",
     margin: "0 auto",
     textAlign: "center",
@@ -20,19 +27,23 @@ export default function RecipeLayout({cardDat}) {
     color: "#02005D",
     fontFamily: "Pacifico",
     fontWeight: "400",
-  }
-
-  const bottomLineStyle = {
+  },
+  bottomLineStyle: {
     margin: "0 30vw",
     paddingBottom: "10px",
     borderTop: "3px solid #02005D",
   }
 
+});
+
+export default function RecipeLayout({cardDat}) {
+  const classes = useStyles();
+
   return(
-    <div style={layoutStyle}>
-      <div style={appBarStyle}>
-        <div style={logoStyle}>My Recipe Book</div>
-        <div style={bottomLineStyle}></div>
+    <div className={classes.layoutStyle}>
+      <div className={classes.appBarStyle}>
+        <div className={classes.logoStyle}>My Recipe Book</div>
+        <div className={classes.bottomLineStyle}></div>
       </div>
       <div>
         <div>
