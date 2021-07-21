a5a75bd46bef4c6528342840783224ce18f2ff20
Sofia-Ortega <s.ortega@tamu.edu>
2021-07-21 14:58:02 -0500

    added stepper to add layout

--- src/components/AddRecipe/AddLayout.js
+++ src/components/AddRecipe/AddLayout.js
@@ -2,14 +2,29 @@ import React from 'react'
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
 import {makeStyles} from "@material-ui/core/styles";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Typography from '@material-ui/core/Typography';
+
 import InputField from "./InputField";
 import {Input} from "@material-ui/core";
 import IngredientInput from "./IngredientInput";
 
-const useStyles = makeStyles( {
+const useStyles = makeStyles( (theme) =>({
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
   title: {
     fontSize: "40px",
-    borderBottom: "1px solid black",
+    //borderBottom: "1px solid black",
     margin: "0 200px",
     padding: "30px 0 15px 0",
     textAlign: "center",
@@ -18,32 +33,83 @@ const useStyles = makeStyles( {
     display: "table",
     margin: "0 auto"
   }
-})
+}));
+
+function getSteps() {
+  return ['Select master blaster campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return 'Select campaign settings...';
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
 
 export default function AddLayout() {
   const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
   return (
-    <div>
+    <div className={classes.root}>
       <div className={classes.title}>
         Add Recipe
       </div>
-        <InputField title={"Recipe Title"}/>
-        <InputField title={"Description"} />
-        <div>Drop down menu of type</div>
-
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
       <div>
-        Ingredients:
-        <IngredientInput />
-        <br />
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
       </div>
-      <div>
-        Notes: (optional)
-      </div>
-     <Link to="/">
-       <Button variant="contained" color="primary" className={classes.center}>
-         Main Page
-       </Button>
-     </Link>
+       <Link to="/">
+         <Button variant="contained" color="primary" className={classes.center}>
+           Main Page
+         </Button>
+       </Link>
     </div>
   )
 }
